fa84dae4ea9e7a46511fb9d3d82ebee3663d39ce
georgegiam <giamourudus22@gmail.com>
2021-03-23 07:31:39 +0000

    navbar frontend

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -25,31 +25,38 @@ const Nav = (props) => {
     return (
         <Router>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">University of Southampton Book Exchange</a>
+            <Link className="navbar-brand" to="/">University of Southampton Book Exchange</Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">               
-                    <li className="nav-item">
-                        <Link to="/addbook">Add Book</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link to="/">Books</Link>
-                    </li>
+                    {/* any other link goes here */}
                 </ul> 
-                <button href="#" className="text-decoration-none" onClick={signOutHandler}>Logout</button>        
-                <Link to="/login" href="#" className="text-decoration-none">Login</Link>                              
+                <div class="dropdown">
+                    <button class="btn btn-secondary btn-sm dropdown-toggle">user@soton.ac.uk</button>
+                    <div class="dropdown-content">
+                        <a class="dropdown-item" href="#">Notifications <span class="badge badge-danger">9</span></a>
+                        <Link class="dropdown-item" to="/addbook">Add Book</Link>
+                        <Link class="dropdown-item" to="/">My Books</Link>
+                        <a class="dropdown-item" href="#">My Calendar</a>
+                        <a class="dropdown-item text-danger" onClick={signOutHandler} href="#">Logout</a>
+                    </div>
+                </div>   
+                {/* <Link to="/login" href="#" className="text-decoration-none">Login</Link> */}
             </div>
         </nav>
         <Switch>
+            {/* route to login */}
             <Route path="/login">
               <LoginForm/>
             </Route>
+            {/* route to addbook */}
             <Route path="/addbook">
               <CreateListing/>
             </Route>
+            {/* route to index */}
             <Route path="/">
                 {props.index}
             </Route>
