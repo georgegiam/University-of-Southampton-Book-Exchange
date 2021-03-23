e681884ebdad8999333769d99a5719f860cbe592
georgegiam <giamourudus22@gmail.com>
2021-03-23 09:13:31 +0000

    add book frontend fixed

--- src/Books/CreateListing.js
+++ src/Books/CreateListing.js
@@ -90,7 +90,7 @@ class CreateListing extends PureComponent {
                         <div class="form-group row">
                             <label for="inputEmail3" class="col-sm-2 col-form-label">Title</label>
                             <div class="col-sm-10">
-                            <input type="text" className="form-control" id="bookName" name="bookName" aria-describedby="bookHelp" placeholder="Enter book title" onChange={this.changeHandler} size="20"/>
+                            <input type="text" className="form-control" id="bookName" name="bookName" aria-describedby="bookHelp" placeholder="Enter book title" onChange={this.changeHandler} size="20" required/>
                             <small id="emailHelp" class="form-text text-muted">Please add the full book title</small>
                             </div>
                         </div>
@@ -98,7 +98,7 @@ class CreateListing extends PureComponent {
                         <div class="form-group row">
                             <label for="inputPassword3" class="col-sm-2 col-form-label">Price</label>
                             <div class="col-sm-10">
-                            <input type="number" className="form-control" id="price" name="price" aria-describedby="priceHelp" min="0" placeholder="Enter book price" onChange={this.changeHandler}/>
+                            <input type="number" className="form-control" id="price" name="price" aria-describedby="priceHelp" min="0" placeholder="Enter book price" onChange={this.changeHandler} required/>
                             <small id="emailHelp" class="form-text text-muted">Price is in £</small>
                             </div>
                         </div>
@@ -109,28 +109,28 @@ class CreateListing extends PureComponent {
                             <div class="col-sm-10">
                                 {/* used */}
                                 <div class="form-check form-check-inline">
-                                <input className="form-check-input" type="radio" value="Used" checked={this.state.bookCondition === 'Used'} onChange={this.handleCondtionChange}/>
+                                <input className="form-check-input" type="radio" value="Used" checked={this.state.bookCondition === 'Used'} onChange={this.handleCondtionChange} required/>
                                 <label class="form-check-label" for="gridRadios1">
                                     Used
                                 </label>
                                 </div>
                                 {/* good */}
                                 <div class="form-check form-check-inline">
-                                <input className="form-check-input" type="radio" value="Good" checked={this.state.bookCondition === 'Good'} onChange={this.handleCondtionChange}/>
+                                <input className="form-check-input" type="radio" value="Good" checked={this.state.bookCondition === 'Good'} onChange={this.handleCondtionChange} required/>
                                 <label class="form-check-label" for="gridRadios2">
                                     Good
                                 </label>
                                 </div>
                                 {/* very good */}
                                 <div class="form-check form-check-inline">
-                                <input className="form-check-input" type="radio" value="Very Good" checked={this.state.bookCondition === 'Very Good'} onChange={this.handleCondtionChange}/>
+                                <input className="form-check-input" type="radio" value="Very Good" checked={this.state.bookCondition === 'Very Good'} onChange={this.handleCondtionChange} required/>
                                 <label class="form-check-label" for="gridRadios3">
                                     Very Good
                                 </label>
                                 </div>
                                 {/* new */}
                                 <div class="form-check form-check-inline">
-                                <input className="form-check-input" type="radio" value="New" checked={this.state.bookCondition === 'New'} onChange={this.handleCondtionChange}/>
+                                <input className="form-check-input" type="radio" value="New" checked={this.state.bookCondition === 'New'} onChange={this.handleCondtionChange} required/>
                                 <label class="form-check-label" for="gridRadios3">
                                     New
                                 </label>
@@ -143,7 +143,6 @@ class CreateListing extends PureComponent {
                             <label for="inputEmail3" class="col-sm-2 col-form-label">Type</label>
                             <div class="col-sm-10">
                                 <select class="form-control" id="exampleFormControlSelect1">
-                                    <option disabled selected>Select book type</option>
                                     <option>Action and adventure</option>
                                     <option>Alternate history</option>
                                     <option>Anthology</option>
@@ -207,7 +206,7 @@ class CreateListing extends PureComponent {
                         <div class="form-group row">
                             <label for="inputEmail3" class="col-sm-2 col-form-label">Description</label>
                             <div class="col-sm-10">
-                            <textarea className="form-control" rows="3" value={this.state.bookDescription} onChange={this.changeHandler} name='bookDescription' placeholder="Write a description for the product"></textarea>
+                            <textarea className="form-control" rows="3" value={this.state.bookDescription} onChange={this.changeHandler} name='bookDescription' placeholder="Write a description for the product" required></textarea>
                             </div>
                         </div>
                         {/* image */}
