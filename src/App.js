4d573da1523dfa80797defcf57890061a6f65d34
georgegiam <giamourudus22@gmail.com>
2021-03-23 07:40:45 +0000

    temp navbar removed

--- src/App.js
+++ src/App.js
@@ -5,15 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // components
 import Details from './Components/BookDetails'
-import Nav from './Components/Navbar'
 
 function App() {
   return (
-    <div>
-      <Nav />
       <Details />
-    </div>
-    
   );
 }
 
