4749568f42c5f5c8676adb417b54f78ae2d44cc8
George Giamouridis <giamouridis22@gmail.com>
2021-04-07 10:52:16 +0100

    update my purchases UI

--- src/Components/myPurchases.js
+++ src/Components/myPurchases.js
@@ -32,24 +32,31 @@ class MyPurchases extends Component {
         console.log("My purchases: ", this.state.purchases);
         //console.log("My Exchanges: ", this.state.currentExchange);
 
-    return <div className="list-group"> {this.state.purchases.map((bookItem, index) => {
+    return <div className="container" id="purchases">
+        <div className="container-fluid">
+            <h2>My purchases</h2><hr/>
+        <div className="list-group"> {this.state.purchases.map((bookItem, index) => {
         if(this.state.currentExchange.length > 0) {
             return (
                 <a key={index} href="#" className="list-group-item list-group-item-action flex-column align-items-start active">
-                <div className="d-flex w-100 justify-content-between">
-                <h5 className="mb-1">Book Name: {bookItem.bookName}</h5>
-                <small>Exchange Date: {this.state.currentExchange[index].date} </small>
-                <small>Exchange Time: {this.state.currentExchange[index].time}</small>
-                <small>Status: {this.state.currentExchange[index].status}</small>
-                </div>
-                <p className="mb-1">£{bookItem.bookPrice}</p>
-                <small>Exchange Location: {this.state.currentExchange[index].location}</small>
+                    <div className="d-flex w-100 justify-content-between">
+                        <h5 className="mb-1">{bookItem.bookName}</h5>
+                        <small><span class="badge badge-warning">{this.state.currentExchange[index].status}</span> </small>
+                    </div>
+                    <p className="mb-1">£{bookItem.bookPrice}</p>
+                    <div className="d-flex w-100 justify-content-between">
+                        <small>Exchange Date and Time: {this.state.currentExchange[index].date} {this.state.currentExchange[index].time}</small>
+                    </div>
+                    <small>Exchange Location: {this.state.currentExchange[index].location}</small>
                 </a>
             );
         } 
 
         }) 
     } </div>
+        </div>
+        
+    </div> 
     
     }
 }
