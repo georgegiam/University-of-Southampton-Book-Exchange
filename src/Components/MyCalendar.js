28c8cde0db64eb097b49e4aa7b156a4cfb1023e9
georgegiam <giamourudus22@gmail.com>
2021-04-08 09:24:21 +0100

    reacti icons added

--- src/Components/MyCalendar.js
+++ src/Components/MyCalendar.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import '../App.css'
 
+// react icons
+import { FaLocationArrow, FaStopwatch, FaCalendar, FaMoneyBill } from 'react-icons/fa';
+
 
 function Calendar() {
     return (
@@ -15,10 +18,11 @@ function Calendar() {
                     <div class="col-10">
                         <h3><strong>Book title</strong></h3>
                         <ul class="list-inline">
-                            <li class="list-inline-item"><i class="fa fa-calendar-o" aria-hidden="true"></i> Monday</li>
-                            <li class="list-inline-item"><i class="fa fa-clock-o" aria-hidden="true"></i> 12:30 PM - 2:00 PM</li>
+                            <li class="list-inline-item"><FaCalendar/> Monday</li>
+                            <li class="list-inline-item"><FaStopwatch/> 2:00 PM</li>
+                            <li class="list-inline-item"><FaLocationArrow/> Cafe</li>
                         </ul>
-                        <p>Location</p>
+                        <p>25$</p>
                     </div>
                 </div>
             </div>
