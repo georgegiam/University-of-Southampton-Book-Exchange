85ce9637d3e4753e5bf31e4680277cbe7f2aca3f
georgegiam <giamourudus22@gmail.com>
2021-04-12 17:39:20 +0100

    appointment scheduler ui

--- src/Components/datePicker.js
+++ src/Components/datePicker.js
@@ -38,21 +38,27 @@ class DatePicker extends Component {
     console.log('date picker', this.state.appointment);
 
     return (
-        <form onSubmit={(e) => this.setDateandTimeHandler(e)}>
-          Please select a date and time for the book exhange:
-          <div className="form-group">
-            <input type="date" className="form-control" onChange={this.dateHandler} required />
-          </div>
+      <div className="container" id="scheduler">
+        <div className="container-fluid">
+          <h2>Appointmen scheduler</h2><hr/>
+          <form onSubmit={(e) => this.setDateandTimeHandler(e)}>
+            <p>Please select a date and time for the book exhange</p>
+            <div className="form-group">
+              <input type="date" className="form-control" onChange={this.dateHandler} required />
+            </div>
 
-          <div className="form-group">
-            <input type="time" className="form-control"onChange={this.timeHandler} required/>
-          </div>
+            <div className="form-group">
+              <input type="time" className="form-control"onChange={this.timeHandler} required/>
+            </div>
 
-          <div className="form-group">
-            <input type="text" className="form-control" onChange={this.locationHandler} placeholder="Appointment Location" required/>
-          </div>
-          <button type="submit" className="btn btn-primary">Save</button>
-          </form>
+            <div className="form-group">
+              <input type="text" className="form-control" onChange={this.locationHandler} placeholder="Appointment Location" required/>
+            </div>
+            <button type="submit" className="btn btn-primary">Save</button>
+            </form>
+        </div>
+      </div>
+
         )
     }
 
