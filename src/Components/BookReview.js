1a90cd432a952d790d38329c4c4a341ce028109e
Abdul Lamzini <abd.ll@hotmail.com>
2021-04-12 17:09:41 +0100

    Change class to className

--- src/Components/BookReview.js
+++ src/Components/BookReview.js
@@ -41,50 +41,50 @@ const Review = (props) => {
             <h2>Book Review</h2><hr/>
 
             <form>
-                <div class="form-group row">
-                    <label for="inputEmail3" class="col-sm-2 col-form-label">Book</label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control" id="inputEmail3" value={bookInfo.bookName} disabled/>
+                <div className="form-group row">
+                    <label for="inputEmail3" className="col-sm-2 col-form-label">Book</label>
+                    <div className="col-sm-10">
+                    <input type="text" className="form-control" id="inputEmail3" value={bookInfo.bookName} disabled/>
                     </div>
                 </div>
-                <div class="form-group row">
-                    <label for="inputPassword3" class="col-sm-2 col-form-label">Seller</label>
-                    <div class="col-sm-10">
-                    <input type="text" class="form-control" id="inputPassword3" value={exchangeInfo.sellerName} disabled/>
+                <div className="form-group row">
+                    <label for="inputPassword3" className="col-sm-2 col-form-label">Seller</label>
+                    <div className="col-sm-10">
+                    <input type="text" className="form-control" id="inputPassword3" value={exchangeInfo.sellerName} disabled/>
                     </div>
                 </div>
-                <fieldset class="form-group">
-                    <div class="row">
-                    <legend class="col-form-label col-sm-2 pt-0">Rate</legend>
-                    <div class="col-sm-10">
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" onChange={ratingHandler} value="1"/>
-                            <label class="form-check-label" for="inlineRadio1"><FaStar/></label>
+                <fieldset className="form-group">
+                    <div className="row">
+                    <legend className="col-form-label col-sm-2 pt-0">Rate</legend>
+                    <div className="col-sm-10">
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" onChange={ratingHandler} value="1"/>
+                            <label className="form-check-label" for="inlineRadio1"><FaStar/></label>
                         </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" onChange={ratingHandler} value="2"/>
-                            <label class="form-check-label" for="inlineRadio2"><FaStar/><FaStar/></label>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" onChange={ratingHandler} value="2"/>
+                            <label className="form-check-label" for="inlineRadio2"><FaStar/><FaStar/></label>
                         </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" onChange={ratingHandler} value="3" />
-                            <label class="form-check-label" for="inlineRadio3"><FaStar/><FaStar/><FaStar/></label>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" onChange={ratingHandler} value="3" />
+                            <label className="form-check-label" for="inlineRadio3"><FaStar/><FaStar/><FaStar/></label>
                         </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" onChange={ratingHandler} value="4" />
-                            <label class="form-check-label" for="inlineRadio3"><FaStar/><FaStar/><FaStar/><FaStar/></label>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" onChange={ratingHandler} value="4" />
+                            <label className="form-check-label" for="inlineRadio3"><FaStar/><FaStar/><FaStar/><FaStar/></label>
                         </div>
-                        <div class="form-check form-check-inline">
-                            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" onChange={ratingHandler} value="5" />
-                            <label class="form-check-label" for="inlineRadio3"><FaStar/><FaStar/><FaStar/><FaStar/><FaStar/></label>
+                        <div className="form-check form-check-inline">
+                            <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" onChange={ratingHandler} value="5" />
+                            <label className="form-check-label" for="inlineRadio3"><FaStar/><FaStar/><FaStar/><FaStar/><FaStar/></label>
                         </div>
                     </div>
                     </div>
                 </fieldset>
-                <div class="form-group row">
-                    <label for="inputEmail3" class="col-sm-2 col-form-label">Review</label>
-                    <div class="col-sm-10">
-                    <textarea class="form-control" onChange={reviewTextHandler} id="exampleFormControlTextarea1" rows="4"></textarea><br/>
-                    <button type="submit" onClick={reviewHandler} class="btn btn-primary">Submit</button>
+                <div className="form-group row">
+                    <label for="inputEmail3" className="col-sm-2 col-form-label">Review</label>
+                    <div className="col-sm-10">
+                    <textarea className="form-control" onChange={reviewTextHandler} id="exampleFormControlTextarea1" rows="4"></textarea><br/>
+                    <button type="submit" onClick={reviewHandler} className="btn btn-primary">Submit</button>
                     </div>
                 </div>
                 </form>
