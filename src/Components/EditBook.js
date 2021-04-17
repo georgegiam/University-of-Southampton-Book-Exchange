7371e23f3e3942343a5ff51fe16f637be198822c
Abdul Lamzini <abd.ll@hotmail.com>
2021-04-17 11:18:45 +0100

    Fixed html tags names

--- src/Components/EditBook.js
+++ src/Components/EditBook.js
@@ -10,18 +10,9 @@ const EditBook = (props) => {
     const [bookPrice, setPrice] = useState(props.location.state.book.bookPrice);
     const [bookDescription, setDescription] = useState(props.location.state.book.bookDescription);
 
-    // var book = null; 
-    // if(props.location.state) {
-    //     book = props.location.state.book;
-    //     console.log("EDIT BOOK", book);
-    //     //setTitle(bookTitle);
-    // }
-    //console.log("EDIT BOOK", props.location.state.book);
     async function saveHandler() {
         var bookID = props.location.state.book.ID;
         await Firebase.updateBookByID(bookID, bookTitle, bookPrice, bookDescription);
-        // console.log(book.bookName);
-        // alert("save Handler pressed");
         const {history} = props; 
         history.push("/myBooks");
     }
@@ -43,19 +34,19 @@ const EditBook = (props) => {
             <div className="container-fluid">
                 <h2>Edit book</h2><hr/>
                 <form>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Book title</label>
-                        <input type="text" class="form-control" id="exampleInputEmail1" onChange={titleHandler} value={bookTitle}/>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">Book title</label>
+                        <input type="text" className="form-control" id="exampleInputEmail1" onChange={titleHandler} value={bookTitle}/>
                     </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Price</label>
-                        <input type="number" min="0" class="form-control" onChange={priceHandler} id="exampleInputPassword1" value={bookPrice}/>
+                    <div className="form-group">
+                        <label htmlFor="exampleInputPassword1">Price</label>
+                        <input type="number" min="0" className="form-control" onChange={priceHandler} id="exampleInputPassword1" value={bookPrice}/>
                     </div>
-                    <div class="form-group">
-                        <label for="exampleFormControlTextarea1">Description</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" onChange={descriptionHandler} value={bookDescription} rows="4"></textarea>
+                    <div className="form-group">
+                        <label htmlFor="exampleFormControlTextarea1">Description</label>
+                        <textarea className="form-control" id="exampleFormControlTextarea1" onChange={descriptionHandler} value={bookDescription} rows="4"></textarea>
                     </div>
-                    <button type="submit" onClick={saveHandler} class="btn btn-primary">Save Changes</button>
+                    <button type="submit" onClick={saveHandler} className="btn btn-primary">Save Changes</button>
                 </form>
             </div>
         </div>
